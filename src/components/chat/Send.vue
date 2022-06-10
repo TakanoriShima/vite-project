39e9d2ad9076275b3010ec1f7f065400f2acc0ce
TakanoriShima <quark2galaxy@gmail.com>
2022-06-10 11:24:44 +0900

    ダミーチャット、送信フォーム表示

---
+++ src/components/chat/Send.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="input-group mb-3">
+    <input
+      type="text"
+      class="form-control"
+      placeholder="メッセージを入力してください"
+    />
+    <button
+      class="btn btn-outline-secondary js-send-message"
+      type="button"
+    >
+      メッセージ送信
+    </button>
+  </div>
+</template>
---
+++ src/components/pages/Chat.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineComponent, reactive } from 'vue'
+import View from '../chat/View.vue' // 追加
+import Send from '../chat/Send.vue'
+
+export default defineComponent({
+  components: {
+    View,
+    Send
+  },
+  setup() {
+    const data = reactive({
+      user: {},
+      chat: [],
+      input: '',
+      displayName: ''
+    })
+    return {
+      data
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="container">
+    <View />
+    <Send />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
